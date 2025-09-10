e142ffba3c453ef17dfa1ac3f516890d8a2aca3d
robineisepoptahti <155745755+robineisepoptahti@users.noreply.github.com>
2025-09-10 13:43:23 +0300

    auto --fix

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 
 // get the port from env variable/ Changed the default to 5001 since 5000 is reserved for apple sharing in IOS
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5001
 
-app.use(express.static("dist"));
+app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
-});
+  console.log(`server started on port ${PORT}`)
+})
