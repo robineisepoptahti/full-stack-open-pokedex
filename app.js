e49c7ff713c1333ce4a85d347cf468e75a1e913e
robineisepoptahti <155745755+robineisepoptahti@users.noreply.github.com>
2025-09-10 14:07:40 +0300

    linted

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
 
 // get the port from env variable/ Changed the default to 5001 since 5000 is reserved for apple sharing in IOS
+
 const PORT = process.env.PORT || 5001
 
 app.use(express.static('dist'))
