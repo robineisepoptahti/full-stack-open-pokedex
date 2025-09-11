1375a3cb5604bac07004fdb7756cf95bb6314e60
robineisepoptahti <155745755+robineisepoptahti@users.noreply.github.com>
2025-09-11 11:05:20 +0300

    Added another test

--- test/e2e-tests/Pokedex.test.js
+++ test/e2e-tests/Pokedex.test.js
@@ -1,13 +1,19 @@
-const { test, describe, expect } = require('@playwright/test')
+const { test, describe, expect } = require("@playwright/test");
 
-describe('Pokedex', () => {
-  test('front page can be opened', async ({ page }) => {
-    await page.goto('')
-    await expect(page.getByText('ivysaur')).toBeVisible()
+describe("Pokedex", () => {
+  test("front page can be opened", async ({ page }) => {
+    await page.goto("");
+    await expect(page.getByText("ivysaur")).toBeVisible();
     await expect(
       page.getByText(
-        'Pokémon and Pokémon character names are trademarks of Nintendo.'
+        "Pokémon and Pokémon character names are trademarks of Nintendo."
       )
-    ).toBeVisible()
-  })
-})
+    ).toBeVisible();
+  });
+
+  test("text is shown on a opened page", async ({ page }) => {
+    await page.goto("");
+    await page.getByText("ivysaur").click();
+    await expect(page.getByText("chlorophyll")).toBeVisible();
+  });
+});
