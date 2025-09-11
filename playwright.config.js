567d6dcf6cb664e1fb84192ab4f2eb3009e3ec46
robineisepoptahti <155745755+robineisepoptahti@users.noreply.github.com>
2025-09-11 10:15:29 +0300

    e2e test added

---
+++ playwright.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "./test/e2e-tests",
+  timeout: 30000,
+  retries: 0,
+  webServer: {
+    command: "npm run start",
+    url: "http://localhost:8080",
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
+  },
+  use: {
+    headless: true,
+    baseURL: "http://localhost:8080/",
+  },
+});
